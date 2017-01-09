c0eb5dc62e61bdacfb05c6cd05e0ce32cbf473f2
Wayne Franklin <waf1waf@gmail.com>
2017-01-09 10:15:10 -0500

    Add check for existing oh-my-zsh

--- getohmyzsh.sh
+++ getohmyzsh.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -d "$HOME/.oh-my-zsh" ]; then
+    echo "You already have oh-my-zsh"
+    exit 1
+fi
+
 pushd .
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 popd
+
