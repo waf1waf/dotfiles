70e4235707bfc3190d154759d8dd5fe6c059f073
Wayne Franklin <waf1waf@gmail.com>
2017-01-09 10:22:00 -0500

    Fix agnoster theme update

--- getohmyzsh.sh
+++ getohmyzsh.sh
@@ -9,3 +9,6 @@ pushd .
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 popd
 
+cp agnoster.zsh-theme $HOME/.oh-my-zsh/themes/
+mv $HOME/.zshrc /tmp/zshrc
+ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
