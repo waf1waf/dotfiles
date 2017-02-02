3895f251066aae3a3de88e12c500236e4c491e8c
Wayne Franklin <wayne.franklin@tridsys.com>
2017-02-02 08:56:01 -0500

    Improve getohmyzsh.sh

--- getohmyzsh.sh
+++ getohmyzsh.sh
@@ -6,9 +6,10 @@ if [ -d "$HOME/.oh-my-zsh" ]; then
 fi
 
 pushd .
+cd
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 popd
 
-cp agnoster.zsh-theme $HOME/.oh-my-zsh/themes/
+cp $HOME/dotfiles/agnoster.zsh-theme $HOME/.oh-my-zsh/themes/
 mv $HOME/.zshrc /tmp/zshrc
 ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
