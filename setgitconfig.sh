b3a143f28ea4f9ef7a21c0edbb5767393b6717de
Wayne Franklin <wayne.franklin@tridsys.com>
2017-06-15 06:21:23 -0400

    Create separate gitconfigs for work and home and a script to set .gitconfig

---
+++ setgitconfig.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+	echo "$0 [work|home]"
+elif [ $1 == "work" ]; then
+	rm ~/.gitconfig
+	ln -s ~/dotfiles/gitconfig-work ~/.gitconfig
+elif [ $1 == "home" ]; then
+	rm ~/.gitconfig
+	ln -s ~/dotfiles/gitconfig-home ~/.gitconfig
+else
+	echo "$0 [work|home]"
+fi
