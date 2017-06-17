0eb79dd288165060cd73d18f760b9ad3529541b3
Wayne Franklin <wayne.franklin@tridsys.com>
2017-06-17 19:40:08 +0000

    Don't fail if no .gitignore

--- setgitconfig.sh
+++ setgitconfig.sh
@@ -3,10 +3,10 @@
 if [ $# -ne 1 ]; then
 	echo "$0 [work|home]"
 elif [ $1 == "work" ]; then
-	rm ~/.gitconfig
+	rm -f ~/.gitconfig
 	ln -s ~/dotfiles/gitconfig-work ~/.gitconfig
 elif [ $1 == "home" ]; then
-	rm ~/.gitconfig
+	rm -f ~/.gitconfig
 	ln -s ~/dotfiles/gitconfig-home ~/.gitconfig
 else
 	echo "$0 [work|home]"
