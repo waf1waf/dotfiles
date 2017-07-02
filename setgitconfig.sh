d2d6716762deef72bee00b2eedac8cd97de627a0
Wayne Franklin <waf1waf@gmail.com>
2017-07-02 18:34:47 +0000

    Make dotfile directories hidden

--- setgitconfig.sh
+++ setgitconfig.sh
@@ -4,10 +4,10 @@ if [ $# -ne 1 ]; then
 	echo "$0 [work|home]"
 elif [ $1 == "work" ]; then
 	rm -f ~/.gitconfig
-	ln -s ~/dotfiles/gitconfig-work ~/.gitconfig
+	ln -s ~/.dotfiles/gitconfig-work ~/.gitconfig
 elif [ $1 == "home" ]; then
 	rm -f ~/.gitconfig
-	ln -s ~/dotfiles/gitconfig-home ~/.gitconfig
+	ln -s ~/.dotfiles/gitconfig-home ~/.gitconfig
 else
 	echo "$0 [work|home]"
 fi
