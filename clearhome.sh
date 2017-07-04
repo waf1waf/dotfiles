ab81e6259343a8541b3de290b895a81e8810aa38
Wayne Franklin <waf1waf@gmail.com>
2017-07-04 12:05:52 +0000

    These have been merged into makesymlinks.sh

--- clearhome.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rm -rf /home/waynef/Desktop /home/waynef/Documents /home/waynef/Downloads /home/waynef/Music /home/waynef/Pictures /home/waynef/Public /home/waynef/Templates /home/waynef/Videos
